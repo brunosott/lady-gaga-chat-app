e4f68a4b893b0642e19fda8a43fff7edf71cb735
Bruno Sottomaior <bruno.mascedo@icloud.com>
2021-02-18 21:29:10 -0300

    kafka config, consumer and endpoint to send message

---
+++ chat-server/src/main/java/br/com/ladygaga/controller/ChatController.java
@@ -0,0 +1,2 @@
+package br.com.ladygaga.controller;public class ChatController {
+}
---
+++ chat-server/src/main/java/br/com/ladygaga/util/KafkaConstant.java
@@ -0,0 +1,10 @@
+package br.com.ladygaga.util;
+
+public class KafkaConstant {
+
+    private KafkaConstant() { }
+
+    public static final String KAFKA_TOPIC = "ladygaga-chat";
+    public static final String GROUP_ID = "ladygaga-group";
+    public static final String KAFKA_BROKER = "localhost:9092";
+}
