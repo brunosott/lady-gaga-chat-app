982622015d5608cddd32e849eea8737145f363ce
Bruno Sottomaior <bruno.mascedo@icloud.com>
2021-02-18 21:47:00 -0300

    Change docker compose

--- chat-server/src/main/java/br/com/ladygaga/consumer/MessageConsumer.java
+++ chat-server/src/main/java/br/com/ladygaga/consumer/MessageConsumer.java
@@ -1,2 +1,25 @@
-package br.com.ladygaga.consumer;public class MessageConsumer {
+package br.com.ladygaga.consumer;
+
+import br.com.ladygaga.model.Message;
+import br.com.ladygaga.util.KafkaConstant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageConsumer {
+
+    @Autowired
+    SimpMessagingTemplate template;
+
+    @KafkaListener(
+            topics = KafkaConstant.KAFKA_TOPIC,
+            groupId = KafkaConstant.GROUP_ID
+    )
+    public void listen(Message message) {
+        System.out.println("sending via kafka listener..");
+        template.convertAndSend("/topic/group", message);
+    }
+
 }
--- chat-server/src/main/java/br/com/ladygaga/controller/ChatController.java
+++ chat-server/src/main/java/br/com/ladygaga/controller/ChatController.java
@@ -1,2 +1,47 @@
-package br.com.ladygaga.controller;public class ChatController {
+package br.com.ladygaga.controller;
+
+import br.com.ladygaga.model.Message;
+import br.com.ladygaga.util.KafkaConstant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.ExecutionException;
+
+@RestController
+public class ChatController {
+
+    @Autowired
+    private KafkaTemplate<String, Message> kafkaTemplate;
+
+    @PostMapping(value = "/api/send", consumes = "application/json", produces = "application/json")
+    public void sendMessage(@RequestBody Message message) {
+        message.setTimestamp(LocalDateTime.now().toString());
+        try {
+            kafkaTemplate.send(KafkaConstant.KAFKA_TOPIC, message).get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/group")
+    public Message broadcastGroupMessage(@Payload Message message) {
+        return message;
+    }
+
+    @MessageMapping("/newUser")
+    @SendTo("/topic/group")
+    public Message addUser(@Payload Message message, SimpMessageHeaderAccessor headerAccessor) {
+        headerAccessor.getSessionAttributes().put("username", message.getSender());
+        return message;
+    }
+
 }
